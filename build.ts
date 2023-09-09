abcd951d53517a36b4f4b1c8996c7eed7c75424f
bhumit070 <ganatrabhoomit070@gmail.com>
2023-09-09 12:06:24 +0530

    UPDATE: build config

--- build.ts
+++ build.ts
@@ -1,8 +1,14 @@
+// delete a directory
+import { rm } from 'fs/promises';
+
+await rm('./dist', { recursive: true, force: true });
+
+await new Promise((r) => setTimeout(r, 1000));
+
 await Bun.build({
-    entrypoints: ['src/index.ts'],
+    entrypoints: ['./src/'],
     outdir: 'dist',
     minify: true,
-    splitting: true,
     target: 'bun',
-    sourcemap: 'external',
+    root: './src',
 });
